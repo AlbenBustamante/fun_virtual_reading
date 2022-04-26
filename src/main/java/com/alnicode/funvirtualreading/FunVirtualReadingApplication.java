cc0fe83a3692d0e8216e1558ac1dd3b66bbca33d
Alben <albensnico@gmail.com>
2022-04-26 15:34:19 -0500

    Initial commit

---
+++ src/main/java/com/alnicode/funvirtualreading/FunVirtualReadingApplication.java
@@ -0,0 +1,13 @@
+package com.alnicode.funvirtualreading;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FunVirtualReadingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FunVirtualReadingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alnicode/funvirtualreading/FunVirtualReadingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alnicode.funvirtualreading;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FunVirtualReadingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
