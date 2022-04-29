8a753f406ea648cb9e3ff354c32c87ae6f7e6759
Alben <albensnico@gmail.com>
2022-04-28 22:50:26 -0500

    Create user service

---
+++ src/main/java/com/alnicode/funvirtualreading/domain/service/impl/UserDetailsServiceImpl.java
@@ -0,0 +1,31 @@
+package com.alnicode.funvirtualreading.domain.service.impl;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import com.alnicode.funvirtualreading.persistence.repository.UserRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service("appUserDetailsService")
+public class UserDetailsServiceImpl implements UserDetailsService {
+    @Autowired
+    private UserRepository repository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        var user = this.repository.findByUsername(username);
+        Set<GrantedAuthority> authorities = new HashSet<>();
+        authorities.add(new SimpleGrantedAuthority(user.getRole()));
+
+        return new User(user.getUsername(), user.getPassword(), authorities);
+    }
+    
+}
