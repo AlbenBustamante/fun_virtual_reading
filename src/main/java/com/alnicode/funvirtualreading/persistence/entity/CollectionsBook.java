5c410fed18c78925b0f4b767c40af4224e4d85c1
Alben <albensnico@gmail.com>
2022-04-28 20:41:22 -0500

    Remove unnecessary import

--- src/main/java/com/alnicode/funvirtualreading/persistence/entity/CollectionsBook.java
+++ src/main/java/com/alnicode/funvirtualreading/persistence/entity/CollectionsBook.java
@@ -5,7 +5,6 @@ import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
-import javax.persistence.MapsId;
 import javax.persistence.Table;
 import javax.validation.constraints.Max;
 import javax.validation.constraints.Min;
