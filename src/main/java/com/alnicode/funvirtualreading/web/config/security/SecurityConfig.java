ff1d9e3bdae7ceb7ec558e5343be18417eea2a06
Alben Bustamante <albenbustamante@gmail.com>
2022-06-16 18:47:57 -0500

    Enable POST requests from /users path

--- src/main/java/com/alnicode/funvirtualreading/web/config/security/SecurityConfig.java
+++ src/main/java/com/alnicode/funvirtualreading/web/config/security/SecurityConfig.java
@@ -2,8 +2,11 @@ package com.alnicode.funvirtualreading.web.config.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 /**
  * Configure the application security.
@@ -23,6 +26,18 @@ public class SecurityConfig {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http.csrf().disable()
+                .authorizeRequests()
+                .antMatchers(HttpMethod.POST, "/users").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .httpBasic()
+                .and()
+                .build();
     }
 
 }
