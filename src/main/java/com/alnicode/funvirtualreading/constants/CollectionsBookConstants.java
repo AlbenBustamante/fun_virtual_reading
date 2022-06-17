57f844f485e268d1f82a86506edda6ad1c6a99d3
Alben Bustamante <albenbustamante@gmail.com>
2022-06-17 18:00:34 -0500

    Define some collections-books constants

--- src/main/java/com/alnicode/funvirtualreading/constants/CollectionsBookConstants.java
+++ src/main/java/com/alnicode/funvirtualreading/constants/CollectionsBookConstants.java
@@ -9,8 +9,25 @@ package com.alnicode.funvirtualreading.constants;
  */
 public final class CollectionsBookConstants {
 
+    /* ===============================
+       -> REQUEST MAPPING PATHS
+       =============================== */
     public static final String COLLECTIONS_BOOKS_PATH = "/collection/{collectionId}/book/{bookId}";
     public static final String MAIN_PATH = "/collection_books";
+    /* =============================== */
+
+    /* ===============================
+       -> VALIDATION MESSAGES
+       =============================== */
+    // BLANK
+    public static final String NOTE_BLANK = "The note cannot be blank";
+    // NULL
+    public static final String RATING_NULL = "The rating cannot be null";
+    // MIN AND MAX
+    public static final String RATING_MIN_MAX = "The rating must be between 1 and 10";
+    // SIZE
+    public static final String NOTE_SIZE = "The note must be between 10 and 200 characters";
+    /* =============================== */
 
     private CollectionsBookConstants() { }
 
