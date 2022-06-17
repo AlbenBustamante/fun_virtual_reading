af6b86de8f056c6eb402a41d455a328034148026
Alben Bustamante <albenbustamante@gmail.com>
2022-06-17 18:12:39 -0500

    Define some nationality constants

---
+++ src/main/java/com/alnicode/funvirtualreading/constants/NationalityConstants.java
@@ -0,0 +1,32 @@
+package com.alnicode.funvirtualreading.constants;
+
+/**
+ * Here is the nationality constants.
+ *
+ * @author Alben Bustamante
+ * @since 1.0
+ * @version 1.0
+ */
+public class NationalityConstants {
+
+    /* ===============================
+       -> EXCEPTION MESSAGES
+       =============================== */
+    public static final String NAME_EXISTS = "This name is already in use, try another";
+    public static final String COUNTRY_EXISTS = "This country is already in use, try another";
+    /* =============================== */
+
+    /* ===============================
+       -> VALIDATION MESSAGES
+       =============================== */
+    // BLANK
+    public static final String NAME_BLANK = "The name cannot be blank";
+    public static final String COUNTRY_BLANK = "The country cannot be blank";
+    // SIZE
+    public static final String NAME_SIZE = "The name must be between 5 and 80 characters";
+    public static final String COUNTRY_SIZE = "The country must be between 3 and 70 characters";
+    /* =============================== */
+
+    private NationalityConstants() { }
+
+}
