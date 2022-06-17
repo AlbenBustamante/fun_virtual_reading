01a08d575879c13766e039d80e35cfe844e926e6
Alben Bustamante <albenbustamante@gmail.com>
2022-06-17 17:47:32 -0500

    Define some book constants

---
+++ src/main/java/com/alnicode/funvirtualreading/constants/BookConstants.java
@@ -0,0 +1,41 @@
+package com.alnicode.funvirtualreading.constants;
+
+/**
+ * Here is the book constants.
+ *
+ * @author Alben Bustamante
+ * @since 1.0
+ * @version 1.0
+ */
+public class BookConstants {
+
+    /* ===============================
+       EXCEPTION MESSAGES
+    -> =============================== */
+    public static final String TITLE_EXISTS = "This title is already in use, try another";
+    public static final String SYNOPSIS_EXISTS = "This synopsis is already in use, try another";
+    public static final String BODY_EXISTS = "The body is already in use, try another";
+    /* ============================ <- */
+
+    /* ===============================
+       VALIDATION MESSAGES
+    -> =============================== */
+    // BLANK
+    public static final String TITLE_BLANK = "The title cannot be blank";
+    public static final String SYNOPSIS_BLANK = "The synopsis cannot be blank";
+    public static final String BODY_BLANK = "The body cannot be blank";
+    // NULL
+    public static final String USER_NULL = "The user cannot be null";
+    public static final String GENRE_NULL = "The genre cannot be null";
+    // MIN
+    public static final String USER_MIN = "The user ID must be at least 1";
+    public static final String GENRE_MIN = "The genre ID must be at least 1";
+    // SIZE
+    public static final String TITLE_SIZE = "The title must be between 10 and 130 characters";
+    public static final String SYNOPSIS_SIZE = "The synopsis must be between 100 and 600 characters";
+    public static final String BODY_SIZE = "The body must be between 1000 and 4000 characters";
+    /* ============================ <- */
+
+    private BookConstants() { }
+
+}
