69015dff5f16427e479e47e35c02f92d4667e872
Alben Bustamante <albenbustamante@gmail.com>
2022-06-17 18:04:57 -0500

    Define some comment constants

---
+++ src/main/java/com/alnicode/funvirtualreading/constants/CommentConstants.java
@@ -0,0 +1,29 @@
+package com.alnicode.funvirtualreading.constants;
+
+/**
+ * Here is the comment constants.
+ *
+ * @author Alben Bustamante
+ * @since 1.0
+ * @version 1.0
+ */
+public class CommentConstants {
+
+    /* ===============================
+       -> VALIDATION MESSAGES
+       =============================== */
+    // BLANK
+    public static final String BODY_BLANK = "The body cannot be blank";
+    // NULL
+    public static final String USER_NULL = "The user cannot be null";
+    public static final String BOOK_NULL = "The book cannot be null";
+    // MIN
+    public static final String USER_MIN = "The user ID must be at least 1";
+    public static final String BOOK_MIN = "The book ID must be at least 1";
+    // SIZE
+    public static final String BODY_SIZE = "The body must be between 1 and 400 characters";
+    /* =============================== */
+
+    private CommentConstants() { }
+
+}
