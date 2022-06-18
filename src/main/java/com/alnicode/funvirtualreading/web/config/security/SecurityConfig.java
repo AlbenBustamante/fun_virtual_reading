129a03592e72893178062851884c39470759b9fd
Alben Bustamante <albenbustamante@gmail.com>
2022-06-17 20:55:47 -0500

    Define the authentication manager bean

--- src/main/java/com/alnicode/funvirtualreading/web/config/security/SecurityConfig.java
+++ src/main/java/com/alnicode/funvirtualreading/web/config/security/SecurityConfig.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -34,6 +36,18 @@ public class SecurityConfig {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    /**
+     * Define an {@link AuthenticationManager} bean.
+     *
+     * @param auth the auth configuration
+     * @return the authentiation manager
+     * @throws Exception if an error occurs
+     */
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration auth) throws Exception {
+        return auth.getAuthenticationManager();
     }
 
     /**
