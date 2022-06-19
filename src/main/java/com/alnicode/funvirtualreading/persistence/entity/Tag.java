472d36b38b80fd996af015522bd16db64ce630bb
Alben Bustamante <albenbustamante@gmail.com>
2022-06-19 02:15:06 -0500

    Create the tag entity

---
+++ src/main/java/com/alnicode/funvirtualreading/persistence/entity/Tag.java
@@ -0,0 +1,50 @@
+package com.alnicode.funvirtualreading.persistence.entity;
+
+import java.util.Objects;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.Hibernate;
+
+/**
+ * The tag model entity.
+ * <p>It will be used to subcategorize a {@link Book} since it is directly related to it</p>
+ *
+ * @author Alben Bustamante
+ * @since 1.0
+ * @version 1.0
+ */
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity
+@Table(name = "tags")
+public class Tag {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "tag_id")
+    private Long id;
+
+    @Column(unique = true, nullable = false, length = 40)
+    private String name;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || Hibernate.getClass(this) != Hibernate.getClass(o)) return false;
+        Tag tag = (Tag) o;
+        return id != null && Objects.equals(id, tag.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return getClass().hashCode();
+    }
+}
